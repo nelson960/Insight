8d66bf055b1dc78109b6a116f591d79b0c774758
Nelson Alex <nelson@Mac.mynet>
2025-07-04 14:20:09 +0100

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+// tailwind.config.mjs
+export default {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}", // You’re using /src/app structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
